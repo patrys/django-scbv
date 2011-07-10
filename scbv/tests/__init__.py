a1c6da0e1531b36a4bca5283e571660b631cd5d1
Patryk Zawadzki <patrys@pld-linux.org>
2011-07-10 20:35:28 +0200

    Handlers are more useful

--- scbv/tests/__init__.py
+++ scbv/tests/__init__.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls.defaults import patterns, include, url
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.http import HttpResponse
@@ -11,23 +11,17 @@ from .. import views
 class Parrot(models.Model):
     name = models.CharField(max_length=20)
 
-    @models.permalink
-    def get_absolute_url(self):
-        return 'form-done', ()
 
-
-class CreateParrot(views.CreateView):
+class ParrotHandler(views.ModelHandler):
     model = Parrot
-
-
-class UpdateParrot(views.CreateView):
-    model = Parrot
+    success_url_create = 'form-done'
+    success_url_update = 'form-done'
+    success_url_delete = 'form-done'
 
 
 urlpatterns = patterns('',
     url(r'^done/$', lambda request: HttpResponse(''), name='form-done'),
-    url(r'^add/$', CreateParrot(), name='create-parrot'),
-    url(r'^(?P<pk>\d+)/$', UpdateParrot(), name='update-parrot'),
+    url(r'^parrot/', include(ParrotHandler().get_urls(prefix='parrot'))),
 )
 
 
@@ -38,25 +32,83 @@ class ClassBasedViewTest(TestCase):
         self.ORIGINAL_TEMPLATE_DIRS = settings.TEMPLATE_DIRS
         settings.TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__),
                                                'templates')]
+        self.test_parrot = Parrot.objects.create(name='test')
 
     def tearDown(self):
         settings.TEMPLATE_DIRS = self.ORIGINAL_TEMPLATE_DIRS
+        self.test_parrot.delete()
 
-    def test_create_view_get(self):
-        result = self.client.get('/add/')
+    def test_create_view_get_valid(self):
+        create_uri = reverse('parrot-create')
+        result = self.client.get(create_uri)
         self.assertEqual(result.status_code, 200)
 
-    def test_create_view_invalid(self):
+    def test_create_view_post_invalid(self):
         data = {
             'name': '',
         }
-        result = self.client.post('/add/', data=data)
+        create_uri = reverse('parrot-create')
+        result = self.client.post(create_uri, data=data)
         self.assertEqual(result.status_code, 200)
 
-    def test_create_view_valid(self):
+    def test_create_view_post_valid(self):
         data = {
             'name': 'Dead',
         }
-        result = self.client.post('/add/', data=data)
+        initial_count = Parrot.objects.all().count()
+        create_uri = reverse('parrot-create')
+        result = self.client.post(create_uri, data=data)
+        final_count = Parrot.objects.all().count()
         self.assertEqual(result.status_code, 302)
         self.assertRedirects(result, reverse('form-done'))
+        self.assertEqual(final_count, initial_count+1)
+
+    def test_read_view_get_valid(self):
+        read_uri = reverse('parrot-read', args=[self.test_parrot.pk])
+        result = self.client.get(read_uri)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.context['instance'].pk, self.test_parrot.pk)
+
+    def test_read_view_get_invalid(self):
+        read_uri = reverse('parrot-read', args=[999])
+        result = self.client.get(read_uri)
+        self.assertEqual(result.status_code, 404)
+
+    def test_update_view_get_valid(self):
+        update_uri = reverse('parrot-update', args=[self.test_parrot.pk])
+        result = self.client.get(update_uri)
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.context['instance'].pk, self.test_parrot.pk)
+
+    def test_update_view_post_invalid(self):
+        data = {
+            'name': '',
+        }
+        update_uri = reverse('parrot-update', args=[self.test_parrot.pk])
+        result = self.client.post(update_uri, data=data)
+        self.assertEqual(result.status_code, 200)
+
+    def test_update_view_post_valid(self):
+        data = {
+            'name': 'Dead',
+        }
+        update_uri = reverse('parrot-update', args=[self.test_parrot.pk])
+        result = self.client.post(update_uri, data=data)
+        self.assertEqual(result.status_code, 302)
+        self.assertRedirects(result, reverse('form-done'))
+        updated_parrot = Parrot.objects.get(pk=self.test_parrot.pk)
+        self.assertEqual(updated_parrot.name, data['name'])
+
+    def test_delete_view_get_invalid(self):
+        delete_uri = reverse('parrot-delete', args=[self.test_parrot.pk])
+        result = self.client.get(delete_uri)
+        self.assertEqual(result.status_code, 403)
+
+    def test_delete_view_post_valid(self):
+        delete_uri = reverse('parrot-delete', args=[self.test_parrot.pk])
+        initial_count = Parrot.objects.all().count()
+        result = self.client.post(delete_uri, data={})
+        final_count = Parrot.objects.all().count()
+        self.assertEqual(result.status_code, 302)
+        self.assertRedirects(result, reverse('form-done'))
+        self.assertEqual(final_count, initial_count-1)
--- scbv/views.py
+++ scbv/views.py
@@ -1,7 +1,8 @@
+from django.conf.urls.defaults import patterns, url
 from django.core.exceptions import ImproperlyConfigured
-from django.db.models import Q
 from django.forms.models import modelform_factory
-from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 
 class View(object):
@@ -10,20 +11,37 @@ class View(object):
     def __call__(self, request):
         return self.respond(self.get_context_data(request))
 
-    def get_template_names(self):
+    def get_template_names(self, **kwargs):
         return [self.template_name]
 
     def get_context_data(self, request, **kwargs):
         return kwargs
 
-    def respond(self, request, context):
-        return TemplateResponse(request, self.get_template_names(), context)
+    def respond(self, request, context, **kwargs):
+        return TemplateResponse(request, self.get_template_names(**kwargs),
+                                context)
 
 class BaseFormView(View):
     form_class = None
+    success_url = None
 
-    def get_form_classes(self):
-        return [self.form_class]
+    def __call__(self, request):
+        return self.handle(request)
+
+    def handle(self, request, **kwargs):
+        forms = self.create_forms(request.POST or None,
+                                  request.FILES or None,
+                                  **kwargs)
+        if self.is_valid(forms, **kwargs):
+            return self.done(forms, **kwargs)
+        return self.respond(request,
+                            self.get_context_data(request,
+                                                  forms=forms,
+                                                  **kwargs),
+                            **kwargs)
+
+    def get_form_class(self, **kwargs):
+        return self.form_class, {}
 
     def get_success_redirect(self, **kwargs):
         if not self.success_url:
@@ -32,22 +50,21 @@ class BaseFormView(View):
         return self.success_url
 
     def create_form(self, form_class, data, files, **kwargs):
-        return form_class(data, files, **kwargs)
+        form_class, extra_args = self.get_form_class(**kwargs)
+        return form_class(data, files, **extra_args)
 
     def create_forms(self, data, files, **kwargs):
-        form_classes = self.get_form_classes()
-        return [self.create_form(form_class, data, files, **kwargs)
-                for form_class in form_classes]
+        return [self.create_form(data, files, **kwargs)]
 
-    def is_valid(self, forms):
+    def is_valid(self, forms, **kwargs):
         return all(f.is_valid() for f in forms)
 
-    def process(self, forms):
+    def process(self, forms, **kwargs):
         raise NotImplementedError()
 
-    def done(self, forms):
-        results = self.process(forms)
-        return self.get_success_redirect(results)
+    def done(self, forms, **kwargs):
+        results = self.process(forms, **kwargs)
+        return self.get_success_redirect(results, **kwargs)
 
 
 class ModelFormView(BaseFormView):
@@ -56,76 +73,111 @@ class ModelFormView(BaseFormView):
     exclude = None
     template_name_suffix = ''
 
-    def __call__(self, request):
-        forms = self.create_forms(request.POST or None,
-                                  request.FILES or None)
-        if self.is_valid(forms):
-            return self.done(forms)
-        return self.respond(request, self.get_context_data(request,
-                                                           forms=forms))
+    def handle(self, request, instance=None, **kwargs):
+        instance = instance or self.create_instance()
+        return super(ModelFormView, self).handle(request, instance=instance,
+                                                 **kwargs)
 
-    def get_template_names(self):
+    def get_template_names(self, **kwargs):
         if self.template_name:
             return [self.template_name]
         return ['%s/%s%s.html' % (self.model._meta.app_label,
                                   self.model._meta.object_name.lower(),
                                   self.template_name_suffix)]
 
-    def get_form_classes(self):
+    def get_form_class(self, instance, **kwargs):
         if self.form_class:
-            return [self.form_class]
-        return [modelform_factory(self.model, fields=self.fields,
-                                  exclude=self.exclude)]
+            return self.form_class, {}
+        return modelform_factory(self.model, fields=self.fields,
+                                 exclude=self.exclude), {}
 
-    def create_instance(self, form_class):
+    def create_instance(self):
         return self.model()
 
-    def create_form(self, form_class, data, files, instance=None, **kwargs):
-        instance = instance or self.create_instance(form_class)
-        return form_class(data, files, **kwargs)
+    def create_form(self, data, files, instance=None, **kwargs):
+        form_class, extra_args = self.get_form_class(instance=instance,
+                                                     **kwargs)
+        return form_class(data, files, instance=instance, **extra_args)
 
-    def process(self, forms):
+    def process(self, forms, **kwargs):
         return [f.save() for f in forms]
 
 
-class CreateView(ModelFormView):
-    template_name_suffix = '_form'
-
-    def get_success_redirect(self, results):
-        return redirect(results[0])
-
-
-class UpdateView(ModelFormView):
-    kwarg_mapping = (
-        ('pk', 'pk', int),
-    )
+class ModelHandler(ModelFormView):
+    '''
+    A basic CRUD handler
+    '''
     queryset = None
-    template_name_suffix = '_form'
+    success_url_creete = None
+    success_url_update = None
+    success_url_delete = None
+    template_name_suffix_create = '_form'
+    template_name_suffix_read = ''
+    template_name_suffix_update = '_form'
 
-    def get_success_redirect(self, results):
-        return redirect(results[0])
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError('Do not call a ModelHandler directly.')
 
-    def get_queryset(self, **kwargs):
-        queryset = self.queryset or self.model._default_manager.all()
-        query = Q()
-        for arg_name, field_name, adapter in self.kwarg_mapping:
-            arg = kwargs.pop(arg_name)
-            query &= Q(**{field_name: adapter(arg)})
-        if kwargs:
-            raise ValueError('Unknown params passed to UpdateView: %s' %
-                             kwargs.keys())
-        return queryset.filter(query)
+    def create(self, request):
+        return self.handle(request, instance=None, intent='create')
 
-    def get_object(self, **kwargs):
-        return self.get_queryset(**kwargs).get()
-
-    def __call__(self, request, **kwargs):
+    def read(self, request, **kwargs):
         instance = self.get_object(**kwargs)
-        forms = self.create_forms(request.POST or None,
-                                  request.FILES or None,
-                                  instance=instance)
-        if self.is_valid(forms):
-            return self.done(forms)
-        return self.respond(request, self.get_context_data(request,
-                                                           forms=forms,
-                                                           object=instance))
+        return self.respond(request,
+                            self.get_context_data(request,
+                                                  instance=instance),
+                            intent='read')
+
+    def update(self, request, **kwargs):
+        instance = self.get_object(**kwargs)
+        return self.handle(request, instance=instance, intent='update')
+
+    def delete(self, request, **kwargs):
+        if request.method != 'POST':
+            return HttpResponseForbidden('Only POST is allowed in delete().')
+        instance = self.get_object(**kwargs)
+        instance.delete()
+        return self.get_success_redirect(results=None, instance=None,
+                                         intent='delete')
+
+    def get_urls(self, prefix=None):
+        prefix = prefix or '%s-%s' % (self.model._meta.app_label,
+                                      self.model._meta.object_name.lower())
+        return patterns('',
+            url(r'^create/$', self.create, name='%s-create' % prefix),
+            url(r'^(?P<pk>\d+)/$', self.read, name='%s-read' % prefix),
+            url(r'^(?P<pk>\d+)/update/$', self.update, name='%s-update' % prefix),
+            url(r'^(?P<pk>\d+)/delete/$', self.delete, name='%s-delete' % prefix),
+        )
+
+    def get_object(self, pk):
+        return get_object_or_404(self.get_queryset(), pk=pk)
+
+    def get_queryset(self):
+        return self.queryset or self.model._default_manager.all()
+
+    def get_template_names(self, intent, **kwargs):
+        if self.template_name:
+            return [self.template_name]
+        if intent == 'create':
+            suffix = self.template_name_suffix_create
+        elif intent == 'read':
+            suffix = self.template_name_suffix_read
+        elif intent == 'update':
+            suffix = self.template_name_suffix_update
+        else:
+            suffix = ''
+        return ['%s/%s%s.html' % (self.model._meta.app_label,
+                                  self.model._meta.object_name.lower(),
+                                  suffix)]
+
+    def get_success_redirect(self, results, intent, **kwargs):
+        if intent == 'create':
+            return redirect(self.success_url_create or results[0])
+        elif intent == 'update':
+            return redirect(self.success_url_update or results[0])
+        elif intent == 'delete':
+            if not self.success_url_delete:
+                raise ImproperlyConfigured('No URL to redirect to.'
+                                           ' Provide a success_url_delete.')
+            return redirect(self.success_url_delete)
